206eb681340e5e0a860e81a540c1f7231bfdc957
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-05-08 13:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
-
+    const strongElements = document.querySelectorAll('strong');
+    strongElements.forEach(element => {
+        element.style.color = 'rgb(0, 128, 0)';
+    });
 }
-
 
 function return_normal() {
     //Write your code here
-
-    
+    const strongElements = document.querySelectorAll('strong');
+    strongElements.forEach(element => {
+        element.style.color = 'rgb(0, 0, 0)';
+    });
 }
